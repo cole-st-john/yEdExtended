6ceba4517e1c6ee84087bff147876556ccb9ba72
Cole St John <info@colestjohn.com>
2024-04-22 11:50:09 +0200

    changes to examples

--- examples/demo-create-and-open.py
+++ examples/demo-create-and-open.py
@@ -9,13 +9,24 @@ graph1.add_node("b")
 graph1.add_edge("a", "b")
 
 # Add arbitrary graph group detail
-group1 = graph1.add_group(
-    "group 1",
-    shape="rectangle",
-)
+group1 = graph1.add_group("group 1", shape="rectangle")
 group1.add_node("c")
 group1.add_node("d")
 group1.add_edge("c", "d")
 
+# subnested group
+group1_1 = group1.add_group("group1_1")
+
+# sub group elements
+group1_1.add_node("e")
+group1_1.add_node("f")
+group1_1.add_edge("e", "f")
+
+# Complex connections ==============
+# standalone node to 2-time nested node
+graph1.add_edge("a", "e")
+# node to subnested group
+graph1.add_edge("b", "group1_1")
+
 # Storing a graph to file and opening it
-graph_file = graph1.persist_graph("test.graphml").open_with_yed()
+graph_file = graph1.persist_graph("test.graphml", overwrite=True).open_with_yed(force=True)
