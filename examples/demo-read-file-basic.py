083ca7553624dcf7ebc1561689f131be78887987
Cole St John <info@colestjohn.com>
2024-04-22 12:50:34 +0200

    example updates

--- examples/demo-read-file-basic.py
+++ examples/demo-read-file-basic.py
@@ -2,7 +2,7 @@ import yedextended as yed
 
 # Reading of graphml file into python objects # Under Construction
 graph1 = yed.Graph().from_existing_graph("examples/test.graphml")
-graph1.persist_graph("abc.graphml", overwrite=True).open_with_yed()
+graph1.persist_graph("abc.graphml", overwrite=True).open_with_yed(force=True)
 
 # graph1 = yed.Graph().from_existing_graph("examples/test.graphml")
 # print(graph1.stringify_graph())
